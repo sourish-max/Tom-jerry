de5a555624291635d187fb34125dee48b7c023cc
sourish-max <76769790+sourish-max@users.noreply.github.com>
2021-08-04 16:37:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var tom,tomImg1,tomImg2,tomImg3,tomImg4,jerry,jerryImg1,jerryImg2,jerryImg3,canvas,bg;
+function preload() {
+    //load the images here
+    bg=loadImage("images/garden.png");
+    tomImg4=loadAnimation("images/cat1.png");
+    tomImg1=loadAnimation("images/cat2.png");
+    tomImg2=loadAnimation("images/cat3.png");
+    tomImg3=loadAnimation("images/cat4.png");
+    jerry=loadAnimation("images/mouse1.png");
+    jerryImg1=loadAnimation("images/mouse2.png");
+    jerryImg2=loadAnimation("images/mouse3.png");
+    jerryImg3=loadAnimation("images/mouse4.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+    tom=createSprite(870,600);
+    tom.addAnimation("tomSleeping",tomImg4);
+    tom.scale=0.2;
+    jerry=createSprite(200,600);
+    jerry.addAnimation("jerryStanding",jerryImg1);
+    jerry.scale=0.15;
+    
+}
+
+function draw() {
+
+    background(bg);
+    //Write condition here to evalute if tom and jerry collide
+    if(tom.x-jerry.x<(tom.width-jerry.width)/2){
+        tom.velocityX=0;
+        tom.addAnimation("tomlastImg",tomImg3);
+        tom.x=300;
+        tom.scale=0.2;
+        tom.changeAnimation("tomlastImg");
+        jerry.addAnimation("jerrylastImg",jerryImg2);
+        jerry.scale=0.15;
+        jerry.changeAnimation("jerrylastImg");
+    }
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+  if(keyCode===RIGHT_ARROW){
+    tom.velocityX=-5;
+    tom.addAnimation("tomRunning",tomImg2);
+    tom.changeAnimation("tomRunning");
+    jerry.addAnimation("jerryTeasing",jerryImg2);
+    jerry.changeAnimation("jerryTeasing");
+    jerry.frameDelay=25;
+
+  }
+
+
+}
